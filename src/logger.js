9547e61a325b10ef2c2480b28bb3f72f07d34356
MT025 <MT025@github.com>
2021-01-21 16:57:05 +0000

    Log now has date

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,6 @@
 const statuses = []
 function updateStatus (status) {
+  status = new Date().toLocaleString() + ":" + status;
   console.debug(status)
   statuses.unshift(status)
   if (statuses.length > 100) statuses.pop()
